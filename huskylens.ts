9be66a73d09f14c3f30b20e77a99055bb2c16599
hitkuangye <85359381+hitkuangye@users.noreply.github.com>
2021-09-23 23:06:15 +0800

    Update huskylens.ts

--- huskylens.ts
+++ huskylens.ts
@@ -1015,3 +1015,121 @@ namespace huskylens {
     
     
 }
+
+
+//% color=#009b5b icon="\uf1eb" block="CUHK_JC_iCar_WIFI"
+namespace ESP8266ThingSpeak {
+
+    let wifi_connected: boolean = false
+    let thingspeak_connected: boolean = false
+    let last_upload_successful: boolean = false
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 100) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    // wait for certain response from ESP8266
+    function waitResponse(): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
+                result = true
+                break
+            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
+                break
+            }
+            if (input.runningTime() - time > 30000) break
+        }
+        return result
+    }
+
+    /**
+    * Initialize ESP8266 module and connect it to Wifi router
+    */
+    //% block="Initialize ESP32-C3|RX (Tx of micro:bit) %tx|TX (Rx of micro:bit) %rx|Baud rate %baudrate"
+    //% tx.defl=SerialPin.P0
+    //% rx.defl=SerialPin.P1
+    export function Initialize(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+        wifi_connected = false
+        thingspeak_connected = false
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
+        sendAT("AT+RESTORE", 2000) // restore to factory settings
+        sendAT("AT+CWMODE=1", 1000) // set to STA mode
+        sendAT("ATE0", 1000) // echo off
+        sendAT("AT+CWLAPOPT=1,2,-50,1", 1000) // Set the Configuration for the Command AT+CWLAP
+        sendAT("AT+CWLAP", 0) // List Available APs
+        basic.pause(100)
+    }
+
+    /**
+    * connect it to Wifi router
+    */
+    //% block="connectWifi|Wifi SSID = %ssid|Wifi PW = %pw"
+    //% ssid.defl=ssid
+    //% pw.defl=pw
+    export function connectWifi(ssid: string, pw: string) {
+        wifi_connected = false
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
+        wifi_connected = waitResponse()
+        basic.pause(100)
+    }
+    
+      
+    /**
+    * Configure to AP mode
+    */
+    //% block="Set to AP mode"
+    export function SetApMode() {
+        sendAT("AT+RESTORE", 2000) // restore to factory settings
+        sendAT("AT+CWMODE=3", 1000) // set to STA and AP mode
+        sendAT("AT+RST", 2000) // restart
+        sendAT("AT+CIPMUX=1", 1000) // Start AP multi connection
+        sendAT("AT+CIPSERVER=1,808", 1000) // Set the server port to 808
+    }
+    
+    /**
+    * Check if ESP8266 successfully connected to Wifi
+    */
+    //% block="Wifi connected ?"
+    export function isWifiConnected() {
+        return wifi_connected
+    }
+    
+    /**
+    * Check List of available APs
+    */
+    //% block="List of available APs"
+    export function ListOfAvailableAPs(): string {
+        return serial.readString()
+    }
+    
+    /**
+    * Information received by Wifi
+    */
+    //% block="Information received = %data"
+    //% data.defl=0
+    export function InformationReceived(data: string): string {
+        let target_str: string = "\u000D\u000A" + "+IPD,0,1:" + data + "\u000D\u000A"
+        let received_str: string = serial.readString()
+        let received_result: boolean = false
+        if (received_str == target_str) {
+            received_result = true
+        }
+        else {
+            received_result = false
+        }
+        return target_str
+    }
+        
+  
+}
